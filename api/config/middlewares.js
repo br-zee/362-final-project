276fd75893f8d182cee7197cde49d443a155e8f0
bzee2004 <brianzee2004@gmail.com>
2025-04-23 15:50:34 -0700

    awdjnuhbeenj

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -1,41 +1,17 @@
-module.exports = ({ env }) => [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
   {
     name: 'strapi::cors',
     config: {
+      enabled: true,
       origin: ['http://localhost:1337', 'https://tiedandtrue.vercel.app'],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
       headers: ["Content-Type", "Authorization"],
       credentials: true,
     },
   },
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            `https://${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com/`,
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            `https://${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com/`,
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  "strapi::security",
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
