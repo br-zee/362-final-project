afbf9c53192d19b1cd72cbff12a7ab7eb0a330c7
bzee2004 <brianzee2004@gmail.com>
2025-04-24 13:02:32 -0700

    oopsie

--- api/src/api/product/controllers/product.js
+++ api/src/api/product/controllers/product.js
@@ -1,38 +1,9 @@
-// 'use strict';
+'use strict';
 
-// const { createCoreController } = require('@strapi/strapi').factories;
+/**
+ * product controller
+ */
 
-// module.exports = createCoreController('api::product.product', ({ strapi }) => ({
-//   async create(ctx) {
-//     const { data } = ctx.request.body;
-//     console.log("incoming data: ", data)
-//     strapi.log.info("incoming data: ", JSON.stringify(data));
+const { createCoreController } = require('@strapi/strapi').factories;
 
-//     if (data.document_id) {
-//       const existing = await strapi.db.query('api::product.product').findOne({
-//         where: { document_id: data.document_id },
-//       });
-
-//       if (existing) {
-//         // Update instead of creating a new entry
-//         const updated = await strapi.entityService.update('api::product.product', existing.id, {
-//           data,
-//         });
-//         return { data: updated} ;
-//       }
-//     } 
-
-//     // Fallback to default create behavior
-//     const response = await super.create(ctx);
-//     return response;
-//   },
-
-//   async update(ctx) {
-//     const { id } = ctx.params;
-//     const { data } = ctx.request.body;
-//     console.log(data);
-//     strapi.log.info('✏️ Updating product ID:', id);
-  
-//     return await super.update(ctx);
-//   }
-// }));
+module.exports = createCoreController('api::product.product');
--- api/src/api/product/routes/product.js
+++ api/src/api/product/routes/product.js
@@ -1,24 +1,9 @@
-// 'use strict';
+'use strict';
 
-// module.exports = {
-//     routes: [
-//       {
-//         method: 'POST',
-//         path: '/products',
-//         handler: 'product.create', // must match your custom controller
-//         config: {
-//           policies: [],
-//           middlewares: [],
-//         },
-//       },
-//       {
-//         method: 'PUT',
-//         path: '/products/:id',
-//         handler: 'product.update',
-//         config: {
-//           policies: [],
-//           middlewares: [],
-//         },
-//       },
-//     ],
-//   };
+/**
+ * product router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product.product');
